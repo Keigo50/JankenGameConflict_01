b8199991d50473f528020577936a786b25c96de5
keigo sato <playfully1115@gmail.com>
2018-08-31 06:01:42 +0900

    修正しました。

--- JankenGameConflict_01/src/janken/JankenController.java
+++ JankenGameConflict_01/src/janken/JankenController.java
@@ -13,6 +13,10 @@ public class JankenController {
 
      private int score = 0;
 
+     private int win = 0;
+     private int draw = 0;
+     private int lose = 0;
+
 
       private Label cpuHandLabel;
       private Label myHandLabel;
@@ -58,13 +62,12 @@ public class JankenController {
          int r = (myHand - cpuHand + 3) % 3;
 
          if(r == 2) {
-             score +=2;
+             score +=2; win++;
             } else if(r==1) {
-             score -=1;
+             score -=1; lose++;
             } else {
-             score +=1;
+             score +=1; draw++;
             }
-
          return (r == 2) ? "あなたの勝ち！" : ((r == 1) ? "あなたの負け！" : "あいこ！");
      }
      //自分の手・相手の手・じゃんけんの結果を各ラベルに表示
@@ -72,6 +75,6 @@ public class JankenController {
          cpuHandLabel.setText("コンピュータの手: " + hands[cpuHand]);
          myHandLabel.setText("あなたの手: " + hands[myHand]);
          resultLabel.setText("結果: " + getResult(myHand, cpuHand));
-         scoreLabel.setText(" Score:"+score);
+         scoreLabel.setText("Score:"+score+" Win:"+win+" Draw:"+draw+" Lose:"+lose);
      }
 }
